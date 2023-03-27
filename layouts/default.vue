3598cc47c3499578c627effa07b2748884259033
gegaenea@yahoo.com <gegaenea@yahoo.com>
2023-03-27 17:05:23 +0300

    Search bar added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="body">
-   <header class="shadow-sm bg-white">
-    <nav class="container mx-auto p-4 flex justify-between">
+   <header class="shadow-sm">
+    <nav class="container mx-auto p-4 flex justify-between items-center">
       <NuxtLink class="font-bold" to="/">Weather App</NuxtLink>
+      <SearchBar />
       <ul class="flex gap-4">
          <li><NuxtLink to="/">Home</NuxtLink> </li>
          <li><NuxtLink to="/about">About</NuxtLink> </li> 
-         <template v-if="modal">
+         <template v-if="!modal">
           <!-- I am using material ui icons here -->
            <svg class="day-btn" @click="modeHandler" xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="currentColor" d="M11 4V1h2v3h-2Zm0 19v-3h2v3h-2Zm9-10v-2h3v2h-3ZM1 13v-2h3v2H1Zm17.7-6.3l-1.4-1.4l1.75-1.8l1.45 1.45l-1.8 1.75ZM4.95 20.5L3.5 19.05l1.8-1.75l1.4 1.4l-1.75 1.8Zm14.1 0l-1.75-1.8l1.4-1.4l1.8 1.75l-1.45 1.45ZM5.3 6.7L3.5 4.95L4.95 3.5L6.7 5.3L5.3 6.7ZM12 18q-2.5 0-4.25-1.75T6 12q0-2.5 1.75-4.25T12 6q2.5 0 4.25 1.75T18 12q0 2.5-1.75 4.25T12 18Z"></path></svg>
          </template>
@@ -61,10 +62,12 @@ methods: {
 .night{
   background: #000;
   color: #fff;
+  transition: all ease-in-out 300ms;
 }
 .day {
   background: #fff;
   color:#000;
+   transition: all ease-in-out 300ms;
 }
 .day-btn, .night-btn {
   cursor: pointer;
