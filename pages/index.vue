ce73b14e432a3ca13ed72fc8518e4f89e6f2622b
gegaenea@yahoo.com <gegaenea@yahoo.com>
2023-03-30 02:50:03 +0300

    Rerender forced.

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <SearchBar />
-    <WeatherCard />
+    <WeatherCard :key="updateKey"/>
   </div>
 </template>
 
 <script setup>
- 
+import {useStore} from "~/store/store"
+const store = useStore()
+import { getCurrentInstance } from 'vue'
+const instance = getCurrentInstance();
+let updateKey = 0
+// I need to force the update in order to get the component to rerender
+ watch(store, () => {
+  updateKey += 1 
+  instance?.proxy?.$forceUpdate();
+  })
 </script>
 
 <style scoped>
